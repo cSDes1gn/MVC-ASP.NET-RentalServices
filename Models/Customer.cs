2bd9deea2169b6fac891b88704cf1e502fe329ed
Christian <christian911@sympatico.ca>
2020-03-01 15:52:07 -0500

    cshtml changes for razor view with dynamic data annotations from model.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -8,10 +8,26 @@ namespace RentalServices.Models
 {
     public class Customer
     {
-        // Data annotations modify our model parameters
-        [Range(1000, 9999, ErrorMessage = "Please enter a valid Customer ID")]
+        // Data annotations modify our model parameters allow these attributes to be modified across the whoe project
+        [Display(Name = "Customer ID")]
+        [Required(ErrorMessage = "Customer ID required")]
+        [Range(1000, 9999, ErrorMessage = "Please enter a number within the range 1000 and 9999")]
         public int Id { get; set; }
+
+        [Display(Name = "Customer Name")]
+        [Required(ErrorMessage = "Customer name required")]
+        [Range(1000, 9999, ErrorMessage = "Please enter a name")]
         public string Name { get; set; }
 
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password required")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "Password length must be between 10 and 100 characters")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage =" Your passwords do not match")]
+        public string ConfirmPassword { get; set; }
     }
 }
